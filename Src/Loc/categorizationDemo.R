a19ea8945e0975d4e04971c3de635ebcf2c6caf7
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-03-03 12:22:35 -0500

    rename variables and change orgnaization for clarity

--- Src/Loc/categorizationDemo.R
+++ Src/Loc/categorizationDemo.R
@@ -6,42 +6,35 @@ source("Src/Reu/categorizePaths.R")
 #Read in the correlation file, RERs, and Paths produced by the original run RERs step 
 allInsectivoryData = read.csv("Results/allInsectivoryCorrelationFile.csv")
 RERs = readRDS("Results/allInsectivoryRERFile.rds")
+Paths = readRDS("Results/allInsectivoryPathsFile.rds") #Note that this path is uncategorized. 
 
-#Note that this path is uncategorized. 
-Paths = readRDS("Results/allInsectivoryPathsFile.rds")
-
-#plot the uncategorized paths for 
-plotRers(RERs,"KIAA0825", Paths )
-
-#convert the zoonomia names to common names
-CNRers = ZonomNameConvertMatrixCommon(RERs)
+CNRers = ZonomNameConvertMatrixCommon(RERs) #convert the dataset names to common names
 
 #Find indexes with "bat" in the species name 
 CNNames = CNRers[1,]
 names(CNNames)
 bats = grep("bat", names(CNNames))
 
+
 #Read in the binary tree (no paths)
 binaryTree = readRDS("Results/allInsectivoryBinaryForegroundTree.rds")
 plotTree(binaryTree)
 
-
 #This code loads in the premade tree, with the manual annotations I used. 
-premadefunctionTree = readRDS("Results/premadefunctionPathManualFGTree.rds")
-plotTree(premadefunctionTree)
+premadeCategoricalTree = readRDS("Results/premadefunctionPathManualFGTree.rds")
+plotTree(premadeCategoricalTree)
 
-#Convert the tree to a path; as the tree (not path) is what is saved 
-    #premadefunctionPaths = tree2Paths(readRDS("Results/premadefunctionPathManualFGTree.rds"), zonomMaster)
-    #This has been pre-executed, to remove dependency on the zoonomia dataset file. Result is read in below. 
-premadefunctionPaths = readRDS("Results/premadeFunctionPaths.rds")
+#Convert the categorical tree to a path
+#premadeCategoricalPaths = tree2Paths(readRDS("Results/premadefunctionPathManualFGTree.rds"), zonomMaster) #This has been pre-executed, to remove dependency on the zoonomia dataset file. Result is read in below. 
+premadeCategoricalPaths = readRDS("Results/premadeFunctionPaths.rds")
 
 #Plot the RERs 
 plotRers(CNRers,"KIAA0825", Paths, sortrers = T)
-plotRers(CNRers,"KIAA0825", premadefunctionPaths, sortrers = T)
+plotRers(CNRers,"KIAA0825", premadeCategoricalPaths, sortrers = T)
 
 
 plotRers(CNRers,"ZNF292", Paths, sortrers = T)
-plotRers(CNRers,"ZNF292", premadefunctionPaths, sortrers = T)
+plotRers(CNRers,"ZNF292", premadeCategoricalPaths, sortrers = T)
 
 # --- OPTIONAL: TO GENERATE A NEW CATEGORICAL PATH 
 #Read in the zoonomia master file. Ensure that this file is stored in the "Data" directory
@@ -49,10 +42,10 @@ plotRers(CNRers,"ZNF292", premadefunctionPaths, sortrers = T)
 zonomMaster = readRDS("Data/RemadeTreesAllZoonomiaSpecies.rds")
 
 #This line runs the manual selection 
-#The "functionPaths" name can be changed, if a different file wants to be saved. 
+#The "categoricalPaths" name can be changed, if a different file wants to be saved. 
 #A tree with the manual annotations used is provided below, this code is present to show how it is generated.
 #This function saves the *tree* to a file, and returns the path when called
-functionPaths = categorizePaths(binaryTree, zonomMaster, "functionPath", overwrite = T) 
+categoricalPaths = categorizePaths(binaryTree, zonomMaster, "categoricalPaths", overwrite = T) 
 # ------
-plotRers(CNRers,"KIAA0825", functionPaths, sortrers = T)
-plotRers(CNRers,"ZNF292", functionPaths, sortrers = T)
+plotRers(CNRers,"KIAA0825", categoricalPaths, sortrers = T)
+plotRers(CNRers,"ZNF292", categoricalPaths, sortrers = T)
